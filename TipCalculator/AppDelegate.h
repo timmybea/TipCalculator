23bbe1e0baa9d1a4a41f0613b62a5c84e94de434
Tim Beals <tim.beals@gmail.com>
2016-11-11 11:16:53 -0800

    Make new project

---
+++ TipCalculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TipCalculator
+//
+//  Created by Tim Beals on 2016-11-11.
+//  Copyright © 2016 Tim Beals. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
