90c652b7b7c0be168fb2410890266af228ee5f30
Tim Beals <tim.beals@gmail.com>
2016-11-11 12:20:15 -0800

    add text field functionality

--- TipCalculator/ViewController.m
+++ TipCalculator/ViewController.m
@@ -10,15 +10,45 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *tipTextField;
+
+@property (weak, nonatomic) IBOutlet UITextField *billTextField;
+
+@property (weak, nonatomic) IBOutlet UILabel *displayLabel;
+@property (weak, nonatomic) IBOutlet UILabel *displayLabelTotal;
+
+@property (strong, nonatomic) IBOutlet UITapGestureRecognizer *tapGesture;
+
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    self.tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(dismissKeyboard:)];
+    [self.view addGestureRecognizer:self.tapGesture];
+    
 }
 
+- (IBAction)CalculateTipButtonPushed:(UIButton *)sender
+{
+    float billAmt = self.billTextField.text.integerValue;
+    float tipPercent = self.tipTextField.text.integerValue;
+    
+    float tipAmount = (tipPercent / 100) * billAmt;
+    float totalAmount = tipAmount + billAmt;
+    
+    self.displayLabel.text = [NSString stringWithFormat:@"You should tip $%.2f", tipAmount];
+    self.displayLabelTotal.text = [NSString stringWithFormat:@"Your total is: $%.2f", totalAmount];
+
+}
+
+- (void)dismissKeyboard:(UITapGestureRecognizer *)sender
+{
+    [self.view endEditing:YES];
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
